6aff1bac8063b6982883f10eafe47a0c8ee54b49
Nataliya Solina <nataliya.solina@bscmsc.ru>
2025-02-11 17:04:18 +0300

    UserController добавлен метод

--- src/main/java/ru/examples/springdemo/controller/UserController.java
+++ src/main/java/ru/examples/springdemo/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.examples.springdemo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,37 +20,8 @@ public class UserController {
         return userService.save(user);
     }
 
-//    @GetMapping("/tasks")
-//    public Iterable<Task> getAll() {
-//        return (List<Task>) taskRepository.findAll();
-//    }
-//
-//    @GetMapping("/tasks/{id}")
-//    public Task getById(@PathVariable Long id) {
-//        return taskRepository.findById(id).orElse(null);
-//    }
-//
-//    @PutMapping("/tasks/{id}")
-//    public Task putById(@PathVariable Long id, @RequestBody Task task) {
-//        task.setId(id);
-//        return taskRepository.save(task);
-//    }
-//
-//    @DeleteMapping("/tasks/{id}")
-//    public void deleteById(@PathVariable Long id) {
-//        taskRepository.deleteById(id);
-//    }
-//
-//    @PatchMapping("/tasks/{id}")
-//    public Task patchById(@PathVariable Long id) {
-//        Task task = taskRepository.findById(id).orElse(null);
-//        assert task != null;
-//        task.setDone(!task.isDone());
-//        return taskRepository.save(task);
-//    }
-//
-//    @PatchMapping("/tasks/{id}:mark-is-done")
-//    public void patchByIdMark(@PathVariable Long id) {
-//        taskRepository.markIsDone(id);
-//    }
+    @GetMapping("/users/me")
+    public User getCurrentUser() {
+        return userService.getCurrentUser();
+    }
 }
