a165fdda374eacaa8efe025b030327c0a87be119
Nataliya Solina <nataliya.solina@bscmsc.ru>
2025-02-11 13:43:14 +0300

    репозиторий

---
+++ src/main/java/ru/examples/springdemo/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package ru.examples.springdemo.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.examples.springdemo.model.Task;
+import ru.examples.springdemo.model.User;
+
+@Repository
+@Transactional
+public interface UserRepository extends PagingAndSortingRepository<User, Long>, CrudRepository<User, Long> {
+
+//    @Modifying
+//    @Query("UPDATE Task t SET t.done = TRUE WHERE t.id = :id")
+//    void markIsDone(@Param("id") Long id);
+}
