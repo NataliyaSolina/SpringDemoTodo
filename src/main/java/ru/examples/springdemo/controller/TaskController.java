77554eb11025be3852ff4a89da04e049c01128b2
Nataliya Solina <nataliya.solina@bscmsc.ru>
2025-02-12 12:10:39 +0300

    контроллеры

--- src/main/java/ru/examples/springdemo/controller/TaskController.java
+++ src/main/java/ru/examples/springdemo/controller/TaskController.java
@@ -1,29 +1,39 @@
 package ru.examples.springdemo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 import ru.examples.springdemo.model.Task;
 import ru.examples.springdemo.model.User;
 import ru.examples.springdemo.repository.TaskRepository;
 import ru.examples.springdemo.service.UserServiceImpl;
 
-import java.util.List;
-
+@Log4j2
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Tasks", description = "Методы для работы со списками задач")
 public class TaskController {
 
     private final TaskRepository taskRepository;
     private final UserServiceImpl userService;
 
-
     @PostMapping("/tasks")
-    public Task create(@RequestBody Task task) {
+    @Operation(summary = "Создание задачи",
+            description = "Позволяет создать задачу пользователю")
+    public Task create(
+            @RequestBody Task task) {
+        log.debug("Post /tasks task");
         return taskRepository.save(task);
     }
 
     @GetMapping("/tasks")
+    @Operation(summary = "Вывод всех задач",
+            description = "Позволяет вывести всего списка задач")
     public Iterable<Task> getAll() {
         User user = userService.getCurrentUser();
 
@@ -33,23 +43,39 @@ public class TaskController {
         return taskRepository.findTasksByUserId(user.getId());
     }
 
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Сотрудник успешно удалён"),
+            @ApiResponse(responseCode = "404", description = "Сотрудник не найден")
+    })
     @GetMapping("/tasks/{id}")
-    public Task getById(@PathVariable Long id) {
+    @Operation(summary = "Вывод задачи по id",
+            description = "Позволяет вывести задачу по заданному id")
+    public Task getById(
+            @Parameter(
+                    description = "ID задачи, данные по которой запрашиваются",
+                    required = true)
+            @PathVariable Long id) {
         return taskRepository.findById(id).orElse(null);
     }
 
     @PutMapping("/tasks/{id}")
+    @Operation(summary = "Редактирование задачи по id",
+            description = "Позволяет редактировать задачу с заданным id")
     public Task putById(@PathVariable Long id, @RequestBody Task task) {
         task.setId(id);
         return taskRepository.save(task);
     }
 
     @DeleteMapping("/tasks/{id}")
+    @Operation(summary = "Удаление задачи по id",
+            description = "Позволяет удалить задачу по заданному id")
     public void deleteById(@PathVariable Long id) {
         taskRepository.deleteById(id);
     }
 
     @PatchMapping("/tasks/{id}")
+    @Operation(summary = "Редактирование отметки выполнения задачи по id",
+            description = "Позволяет изменить отметку выполнения на противоположную у задачи с заданным id")
     public Task patchById(@PathVariable Long id) {
         Task task = taskRepository.findById(id).orElse(null);
         assert task != null;
@@ -58,6 +84,8 @@ public class TaskController {
     }
 
     @PatchMapping("/tasks/{id}:mark-is-done")
+    @Operation(summary = "Редактирование отметки выполнения задачи по id",
+            description = "Позволяет изменить отметку выполнения на 'Выполнено' у задачи с заданным id")
     public void patchByIdMark(@PathVariable Long id) {
         taskRepository.markIsDone(id);
     }
--- src/main/java/ru/examples/springdemo/controller/UserController.java
+++ src/main/java/ru/examples/springdemo/controller/UserController.java
@@ -1,27 +1,37 @@
 package ru.examples.springdemo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.examples.springdemo.model.User;
-import ru.examples.springdemo.repository.UserRepository;
 import ru.examples.springdemo.service.UserServiceImpl;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Users", description = "Методы для работы с пользователями")
 public class UserController {
 
     private final UserServiceImpl userService;
 
     @PostMapping("/users")
+    @Operation(summary = "Регистрация пользователя",
+            description = "Позволяет создать пользователя")
     public User create(@RequestBody User user) {
+        log.debug("Post /user user {}", user);
         return userService.save(user);
     }
 
     @GetMapping("/users/me")
+    @Operation(summary = "Вывод текущего пользователя",
+            description = "Позволяет получить текущего пользователя")
     public User getCurrentUser() {
+        log.debug("Get /users/me");
         return userService.getCurrentUser();
     }
 }
